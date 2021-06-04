9ce6c3949061c0c1cd2c026548cf2082831db4d5
Isabella Riquetti <riquetti.isabella@gmail.com>
2021-06-03 20:00:45 -0700

    Moving delegate test

---
+++ Debug/Course/Delegate/DelegateProgram.cs
@@ -0,0 +1,16 @@
+﻿using Core.Course.Delegate;
+using System;
+
+namespace Core
+{
+    class DelegateProgram
+    {
+        static void Main(string[] args)
+        {
+            var search = new Search();
+            Logging.Log(search.Search1, new MyRequest(), "Search 1");
+            Logging.Log(search.Search2, new MyRequest(), "Search 2");
+            Console.ReadKey();
+        }
+    }
+}
--- Debug/Program.cs
+++ Debug/Program.cs
@@ -7,22 +7,7 @@ namespace Core
     {
         static void Main(string[] args)
         {
-            var search = new Search();
-            Logging.Log(search.Search1, new MyRequest(), "Search 1");
-            Logging.Log(search.Search2, new MyRequest(), "Search 2");
             Console.ReadKey();
-        }
-
-        public bool Search1(IRequest request)
-        {
-            Console.WriteLine("Search 1");
-            return true;
-        }
-
-        public bool Search2(IRequest request)
-        {
-            Console.WriteLine("Search 2");
-            return false;
         }
     }
 }
